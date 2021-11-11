2c5d8842891fa007a4f6680c88f26afb3adeb2a2
Preent Loo <psilocybit5211@gmail.com>
2021-11-10 22:48:15 -0300

    Clase de operadores binarios o aritmeticos

---
+++ 08-oprdrAritm.js
@@ -0,0 +1,19 @@
+// Tambien llamados operadores binarios
+3 + 2;
+50 - 10;
+10 * 10;
+20 / 2;
+"Region " + "de Tarapaca"; // "Region de Tarapaca". Concatenación
+!false; // true. El signo " ! "  es una negación. Conocido como unitary operator
+var a = 1; // asignacion de valor
+3 == "3"; // true. comparación de valores. NO compara el tipo d valor. Compara el contenido
+3 === "3"; // false. realiza una comparación estricta de valores. Compara el cont y el tipo
+5 < 3; // fasle
+5 > 3; // false
+5 <= 6; // true
+5 >= 6; // false
+a && b; // Si a es verdad Y  b es verdad, se cumple la condición
+a || b; // Si a O b es verdad, se cumple la condición
+var edad = 40
+edad++ // Aumenta el valor en uno
+edad += 2 // Suma el valor de la var edad con el número después del igual
